66b40eda0f0f686550eb81fbd9999f0694106587
Alicja Texler <emunah73@gmail.com>
2022-08-14 13:19:48 -0400

    added camera movement

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+    // Start is called before the first frame update
+    [SerializeField] Transform target;
+    void Update()
+    {
+        transform.position = target.transform.position;
+
+    }
+}
