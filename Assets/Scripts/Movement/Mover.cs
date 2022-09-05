eec4b246fe3251942b510142db88fabc0cdc6ffa
Alicja Texler <emunah73@gmail.com>
2022-09-05 13:22:36 -0400

    refactoring

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -3,32 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Mover : MonoBehaviour
-{
+namespace RPG.Movement {
+  public class Mover : MonoBehaviour {
 
-   [SerializeField] Transform target;
-    void Update()
-    {
-      if (Input.GetMouseButton(0)) {
-        MoveToCursor();   
-      }
+    [SerializeField] Transform target;
+    void Update() {
+      // if (Input.GetMouseButton(0)) {
+      //   MoveToCursor();   
+      // }
       UpdateAnimator();
     }
 
-  private void MoveToCursor() {
+    private void MoveToCursor() {
       Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
       RaycastHit hit;
       bool hasHit = Physics.Raycast(ray, out hit);
       if (hasHit) {
-        GetComponent<NavMeshAgent>().destination = hit.point;
+        MoveTo(hit.point);
       }
+    }
+
+    public void MoveTo(Vector3 destination) {
+      GetComponent<NavMeshAgent>().destination = destination;
+    }
+
+    private void UpdateAnimator() {
+      Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+      Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+      float speed = localVelocity.z;
+      GetComponent<Animator>().SetFloat("ForwardSpeed", speed);
+    }
+
   }
 
-  private void UpdateAnimator(){
-    Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
-    Vector3 localVelocity = transform.InverseTransformDirection(velocity);
-    float speed = localVelocity.z;
-    GetComponent<Animator>().SetFloat("ForwardSpeed", speed);
-  }
-
-}
+} //RPG.Movement
