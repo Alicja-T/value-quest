9b9c98db052b5783685db38f009914f1e89a5eb6
Alicja Texler <emunah73@gmail.com>
2022-09-11 12:58:16 -0400

    updated movement logic

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -14,35 +14,39 @@ namespace RPG.Control {
 
     // Update is called once per frame
     void Update() {
-      InteractWithCombat();
-      InteractWithMovement();
-
+      if (InteractWithCombat()) {
+        return;
+      };
+      if (InteractWithMovement()) {
+        return;
+      }
     }
 
-    private void InteractWithCombat() {
+    private bool InteractWithCombat() {
       RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
-      foreach(RaycastHit hit in hits){
+      foreach (RaycastHit hit in hits) {
         CombatTarget target = hit.transform.gameObject.GetComponent<CombatTarget>();
-        if (target!= null) {
+        if (target != null) {
           if (Input.GetMouseButtonDown(0)) {
-	          GetComponent<Fighter>().Attack(target);
+            GetComponent<Fighter>().Attack(target);
           }
+          return true;
         }
       }
+      return false;
     }
 
-    private void InteractWithMovement() {
-      if (Input.GetMouseButton(0)) {
-        MoveToCursor();
-      }
-    }
+    private bool InteractWithMovement() {
 
-    private void MoveToCursor() {
       RaycastHit hit;
       bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
       if (hasHit) {
-        GetComponent<Mover>().MoveTo(hit.point);
+        if (Input.GetMouseButton(0)) {
+          GetComponent<Mover>().MoveTo(hit.point);
+        }
+        return true;
       }
+      return false;
     }
 
     private static Ray GetMouseRay() {
