710e490328fea8325dd062304930109289f02790
Alicja Texler <emunah73@gmail.com>
2022-10-30 14:35:59 -0400

    added death animation

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -4,12 +4,23 @@ namespace RPG.Combat {
 public class Health : MonoBehaviour {
    
     [SerializeField] float health = 100f;
+    bool isDead = false;
 
        // Update is called once per frame
 
     public void TakeDamage(float damage) {
        health = Mathf.Max(health-damage,0);
-       print(health);
+       if (health == 0) {
+         DeathSequence();
+       }
+    }
+
+
+    void DeathSequence() {
+      if (!isDead) {
+          GetComponent<Animator>().SetTrigger("DeathTrigger");
+          isDead = true;
+      }
     }
  
 }//class
