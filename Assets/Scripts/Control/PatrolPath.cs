37cc9d8923ad92dd8acef8229cb181017d4bfa7c
Alicja Texler <emunah73@gmail.com>
2022-11-02 21:25:23 -0400

    added patrolPath gizmos

---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace RPG.Control {
+public class PatrolPath : MonoBehaviour
+{
+   private void OnDrawGizmos() {
+    for (int i = 0; i < transform.childCount; i++) {
+       Transform childTransform = transform.GetChild(i);
+       Gizmos.color = Color.white;
+       Gizmos.DrawSphere(childTransform.position, 0.3f);
+    }
+   }
+}
+
+}
