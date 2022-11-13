247dc4633d21b96e827d1e5456793934c3dc4c6c
Alicja Texler <emunah73@gmail.com>
2022-11-12 21:22:53 -0500

    trigger cinematics once

--- Assets/Scripts/Cinematics/CinematicsTrigger.cs
+++ Assets/Scripts/Cinematics/CinematicsTrigger.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using RPG.Core;
 namespace RPG.Cinematics {
 public class CinematicsTrigger : MonoBehaviour
-{
+{   
+    bool wasPlayed = false;
     private void OnTriggerEnter(Collider other) {
-        GetComponent<PlayableDirector>().Play();
+        if (!wasPlayed && other.gameObject.tag == CoreConstants.PLAYER_TAG) {
+            GetComponent<PlayableDirector>().Play();
+            wasPlayed = true;
+        }
     }
 }
 }
