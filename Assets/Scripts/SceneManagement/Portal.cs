89c71339753510ce802d16309b564437a26f1cb2
Alicja Texler <emunah73@gmail.com>
2022-11-14 21:08:19 -0500

    async scene load

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -6,10 +6,18 @@ using RPG.Core;
 namespace RPG.SceneManagement {
 public class Portal : MonoBehaviour {
     [SerializeField] int sceneIndex = -1;
+
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == CoreConstants.PLAYER_TAG) {
-            SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single);
+            StartCoroutine(Transition());
+        }    
+    }
+
+    private IEnumerator Transition() {
+        DontDestroyOnLoad(gameObject);
+        yield return SceneManager.LoadSceneAsync(sceneIndex);
+        print("scene loaded");
+        Destroy(gameObject);
         }
     }
-}
 }//namespace
