fe73d3c7a00b84f03b8a776c533b1167a6c58e48
Alicja Texler <emunah73@gmail.com>
2022-11-15 20:22:06 -0500

    implemented spawPoint

--- Assets/Scripts/Core/CoreConstants.cs
+++ Assets/Scripts/Core/CoreConstants.cs
@@ -6,7 +6,7 @@ static class CoreConstants {
     public const string DEATH_TRIGGER = "DeathTrigger";
 
     public const string SANDBOX_2 = "Sandbox_2";
-
+    public const string PORTAL_TAG = "Portal";
 }
 
 
--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 using RPG.Core;
 namespace RPG.SceneManagement {
 public class Portal : MonoBehaviour {
     [SerializeField] int sceneIndex = -1;
+    [SerializeField] Transform spawnPoint;
 
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == CoreConstants.PLAYER_TAG) {
@@ -16,8 +18,28 @@ public class Portal : MonoBehaviour {
     private IEnumerator Transition() {
         DontDestroyOnLoad(gameObject);
         yield return SceneManager.LoadSceneAsync(sceneIndex);
-        print("scene loaded");
+        print("Scene loaded");
+        Portal otherPortal = GetOtherPortal();
+        UpdatePlayer(otherPortal);
         Destroy(gameObject);
-        }
     }
+    private void UpdatePlayer(Portal otherPortal) {
+        GameObject player = GameObject.FindGameObjectWithTag(CoreConstants.PLAYER_TAG);
+        player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+        //player.transform.position = otherPortal.spawnPoint.position;
+        player.transform.rotation = otherPortal.spawnPoint.rotation;
+    }
+
+    private Portal GetOtherPortal() {
+        foreach (Portal portal in FindObjectsOfType<Portal>()) {
+            if (portal == this) continue;
+            return portal;
+        }
+        return null;
+    }
+    
+    
+    }
+
+    
 }//namespace
