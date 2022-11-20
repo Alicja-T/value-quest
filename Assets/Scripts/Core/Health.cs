12e5d35ce0648c941d46804eed9b92cdcac35b38
Alicja Texler <emunah73@gmail.com>
2022-11-20 16:55:36 -0500

    saving health

--- Assets/Scripts/Core/Health.cs
+++ Assets/Scripts/Core/Health.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
+using RPG.Saving;
 
 namespace RPG.Core {
-public class Health : MonoBehaviour {
+public class Health : MonoBehaviour, ISaveable{
    
     [SerializeField] float health = 100f;
     bool isDead = false;
 
-       // Update is called once per frame
-   public bool IsDead() {
+   
+    // Update is called once per frame
+    public bool IsDead() {
       return isDead;
    }
+
+    
 
     public void TakeDamage(float damage) {
        health = Mathf.Max(health-damage,0);
@@ -25,6 +29,17 @@ public class Health : MonoBehaviour {
           GetComponent<Animator>().SetTrigger(CoreConstants.DEATH_TRIGGER);
           GetComponent<ActionScheduler>().CancelCurrentAction();
           isDead = true;
+      }
+    }
+
+    public object CaptureState() {
+      return health;
+    }
+
+    public void RestoreState(object state) {
+      health = (float)state;
+      if (health == 0) {
+         DeathSequence();
       }
     }
 
