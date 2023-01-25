801dda602eac738086028f5fd15647bb4498ee4b
Alicja Texler <emunah73@gmail.com>
2023-01-24 20:39:44 -0500

    added projectile script

---
+++ Assets/Scripts/Combat/Projectile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Core;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] Transform target = null;
+    [SerializeField] float speed = 1f;
+    
+    void Update()
+    {   
+        if (target == null) return;
+        transform.LookAt(GetAimLocation());
+        transform.Translate(Vector3.forward*Time.deltaTime);
+        
+    }
+
+    private Vector3 GetAimLocation(){
+        CapsuleCollider collider = target.GetComponent<CapsuleCollider>();
+        if (collider == null) return target.position;
+        return target.position + Vector3.up * collider.height/2f;
+    }
+}
