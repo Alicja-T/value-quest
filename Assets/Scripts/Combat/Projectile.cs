e2a9b906e7acad822c7b7a506b5709518b88667a
Alicja Texler <emunah73@gmail.com>
2023-02-12 16:01:10 -0500

    improving projectiles

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -7,12 +7,23 @@ public class Projectile : MonoBehaviour
 {
     
     [SerializeField] float speed = 1f;
+    [SerializeField] bool isHoming = false;
     Health target = null;
     float damage = 0f;
+    
+    Vector3 shootingDirection; 
+
+
+    void Start() {
+        transform.LookAt(GetAimLocation());
+    }
+
     void Update()
     {   
         if (target == null) return;
-        transform.LookAt(GetAimLocation());
+        if (isHoming && !target.IsDead()) {
+            transform.LookAt(GetAimLocation());
+        }
         transform.Translate(Vector3.forward*Time.deltaTime*speed);
         
     }
@@ -23,15 +34,17 @@ public class Projectile : MonoBehaviour
     }
 
     private Vector3 GetAimLocation(){
-        CapsuleCollider collider = target.GetComponent<CapsuleCollider>();
+         CapsuleCollider collider = target.GetComponent<CapsuleCollider>();
         if (collider == null) return target.transform.position;
         return target.transform.position + Vector3.up * collider.height/2f;
     }
 
+  
     private void OnTriggerEnter(Collider other){
         if (other.GetComponent<Health>() != target) {
             return;
         }
+        if (target.IsDead()) return;
         target.TakeDamage(damage);
         Destroy(gameObject);
     }
