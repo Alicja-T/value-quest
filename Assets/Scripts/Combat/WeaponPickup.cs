e18d8ee53bb36f2081b3eeaac408f92116d66d6d
Alicja Texler <emunah73@gmail.com>
2023-02-20 10:54:07 -0500

    respawnPickups

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -7,6 +7,7 @@ public class WeaponPickup : MonoBehaviour
 { 
 
     [SerializeField] Weapon weapon = null;
+    [SerializeField] float respawnTime = 5f;
     /// <summary>
     /// OnTriggerEnter is called when the Collider other enters the trigger.
     /// </summary>
@@ -15,9 +16,26 @@ public class WeaponPickup : MonoBehaviour
         
         if (other.gameObject.tag == CoreConstants.PLAYER_TAG) {
             other.GetComponent<Fighter>().EquipWeapon(weapon);
-            Destroy(gameObject);
+            StartCoroutine(HideForSeconds(respawnTime));
         }
     }
-  
+
+    IEnumerator HideForSeconds(float seconds) {
+        ShowPickup(false);
+        yield return new WaitForSeconds(seconds);
+        ShowPickup(true);
+    } 
+    
+    void ShowPickup(bool shouldShow) {
+        GetComponent<Collider>().enabled = shouldShow;
+        foreach( Transform child in transform ){
+            child.gameObject.SetActive(shouldShow);
+        }
+
+    }
+
+    void ShowPickup() {
+
+    }
 }
 }
---
+++ Assets/Scripts/Stats/BaseStats.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Stats {
+public class BaseStats : MonoBehaviour
+{
+    [SerializeField] int level = 1;
+    [SerializeField] string characterClass;
+        
+    
+}
+
+}
